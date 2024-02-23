ef6334464835932499eeb0242d4d1dfdd703e6e6
rachelMae <53448557+rachelMae@users.noreply.github.com>
2024-02-23 18:40:33 -0500

    Update MinotaursVase.java

--- MinotaursVase.java
+++ MinotaursVase.java
@@ -7,7 +7,7 @@ import java.util.concurrent.locks.ReentrantLock;
 public class MinotaursVase {
 
     static int N = 15; // guests or threads at the party
-    static int partyInSession = 0;
+    static int partyInSession = 0; //adjust this to adjust how long the party goes (aka how many times someone gets in the queue)
 
     static Lock lock = new ReentrantLock(); /// type of lock for threads
     static Queue<Integer> showroomQueue = new LinkedList<>(); // FIFO queue for the showroom
@@ -15,17 +15,19 @@ public class MinotaursVase {
 
     public static void main(String[] args) throws InterruptedException {
 
+        //time the party to make sure the program ends
+        //during the party call random threads (guests) to join the queue
        while(partyInSession <= 30){
             Random random = new Random();
             int i = random.nextInt(N) + 1;
             int threadNumber = i;
             Thread thread = new Thread(() -> enterQ(threadNumber));
             thread.start();
-            // delay each thread a random amount to simulate the labyrith
+            // delay each thread a random amount to simulate the party
+            int rand = random.nextInt(1000) + 1;
             try {
-                Thread.sleep(1000);
+                Thread.sleep(rand);
             } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
 
@@ -36,8 +38,12 @@ public class MinotaursVase {
 
         showroomQueue.add(threadNumber);
 
+        //System.out.println(threadNumber + " joins the Queue");
+
         //view the vase
         lock.lock();
+
+        //System.out.println(threadNumber + " enters the showroom");
         enterShowroom(showroomQueue.remove());
 
         lock.unlock();
@@ -45,22 +51,11 @@ public class MinotaursVase {
     }
     private static void enterShowroom(int threadNumber){
 
-        //view the vase
-        //:)
+        //view the vase thread sleep to simulate time viewing
         try {
             Thread.sleep(900);
         } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
-        exitShowroom(threadNumber);
-
     }
-    private static void exitShowroom(int threadNumber){
-
-        //view the vase
-
-    }
-    
 }
